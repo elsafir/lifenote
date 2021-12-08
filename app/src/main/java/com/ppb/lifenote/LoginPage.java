f4855205a9350570ad4bd3cb1142a9a3cf87de9b
kharismaputri <imamfadhkur1@gmail.com>
2021-12-08 13:24:02 +0700

    memfungsikan registrasi via email dan password, dan login via email dan password

--- app/src/main/java/com/ppb/lifenote/LoginPage.java
+++ app/src/main/java/com/ppb/lifenote/LoginPage.java
@@ -1,25 +1,70 @@
 package com.ppb.lifenote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
+
+    TextView toRegister;
+    EditText etUsername, etPassword;
+    Button login;
+    String username, password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
-        TextView toRegister = findViewById(R.id.btn_register);
+        mAuth = FirebaseAuth.getInstance();
+        toRegister = findViewById(R.id.btn_to_page_register);
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        login = findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cekLogin();
+            }
+        });
         toRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginPage.this, RegisterPage.class));
             }
         });
+    }
+
+    private void cekLogin() {
+        username = etUsername.getText().toString();
+        password = etPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(LoginPage.this, "Login berhasil", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginPage.this, "Login gagal, cek username dan password anda", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/ppb/lifenote/RegisterPage.java
+++ app/src/main/java/com/ppb/lifenote/RegisterPage.java
@@ -1,13 +1,28 @@
 package com.ppb.lifenote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText etUsername, etPassword;
+    Button btnRegister;
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +36,37 @@ public class RegisterPage extends AppCompatActivity {
                 startActivity(new Intent(RegisterPage.this, LoginPage.class));
             }
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrasi();
+            }
+        });
+    }
+
+    private void registrasi() {
+        username = etUsername.getText().toString();
+        password = etPassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(RegisterPage.this, "Registrasi berhasil", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterPage.this, "Registrasi gagal", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
